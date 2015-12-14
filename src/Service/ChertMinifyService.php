2c87b57d77dafe353baef844d2f8b8c8ba7cb01c
Pierre-Yves Gillier <pierre-yves@gillier.name>
2015-12-14 10:56:08 +0100

    fix #8 UI updates in status page

--- src/Service/ChertMinifyService.php
+++ src/Service/ChertMinifyService.php
@@ -69,30 +69,46 @@ class ChertMinifyService
 	
 	public function getAll()
 	{
-		$sql = "SELECT * from url";
-		$links = $this->cnx->fetchAll($sql);
+		$qb = $this->cnx->createQueryBuilder()
+			->select('u.id, u.url, u.created_at')
+			->from('url', 'u')
+		;
+		$links = $qb->execute()->fetchAll($sql);
 		
 		return $links;
 	}
 
-	public function getListing($offset, $limit)
+	/**
+	 * Returns a paginated listing of records
+	 *
+	 * Records are ordered by creation date.
+	 *
+	 * @param int $offset Where to start the listing
+	 * @param int $limit
+	 * @return array
+	 */
+	public function getListing($offset=0, $limit=10)
 	{
-		$queryBuilder = $this->cnx->createQueryBuilder();
-
-		$queryBuilder
+		$queryBuilder = $this->cnx->createQueryBuilder()
 			->select('*')
 			->from("url", "u")
+			->orderBy("u.created_at", "ASC")
 			->setFirstResult($limit * $offset)
 			->setMaxResults($limit);
 
 		return $queryBuilder->execute()->fetchAll();
 	}
 	
+	/*
+	 * Return number of links in database
+	 *
+	 * @return int The count
+	 */
 	public function countLinks()
 	{
-		$sql = "SELECT COUNT(*) AS TOTAL from url";
-		$result = $this->cnx->fetchAssoc($sql);
+		$sql = "SELECT COUNT(*) AS total from url";
+		$result = $this->cnx->executeQuery($sql)->fetch();
 		
-		return $result['TOTAL'];
+		return $result['total'];
 	}
 }
